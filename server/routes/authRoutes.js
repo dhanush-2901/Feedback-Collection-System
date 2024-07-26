ac82d25d2fed0a213d0004bfc70389c79029a311
Dhanush Prabu <96951861+dhanush-2901@users.noreply.github.com>
2024-07-26 11:05:01 +0530

    Files Upload

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const {register, login, getAllAdmins} = require("../controllers/authController");
+
+router.post("/register", register);
+router.post("/login", login);
+router.get('/admins', getAllAdmins);
+
+module.exports = router;
